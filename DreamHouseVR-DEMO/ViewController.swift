923f73ade964adc164481800411e8ee73514f3c3
Petr Brantalík <petr.brantalik@ideatech.cz>
2020-04-24 17:25:05 +0200

    configuration ARView

--- DreamHouseVR-DEMO/ViewController.swift
+++ DreamHouseVR-DEMO/ViewController.swift
@@ -8,18 +8,50 @@
 
 import UIKit
 import RealityKit
+import ARKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, ARSessionDelegate {
     
     @IBOutlet var arView: ARView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setupARView()
+                   
+                
+                   
+        arView.session.delegate = self
+                   
+              
         // Load the "Box" scene from the "Experience" Reality File
         let boxAnchor = try! Experience.loadBox()
-        
+
         // Add the box anchor to the scene
         arView.scene.anchors.append(boxAnchor)
     }
+
+    
+//     override func viewDidAppear(_ animated: Bool) {
+//            super.viewDidAppear(animated)
+//
+//            setupARView()
+//
+//
+//
+//            arView.session.delegate = self
+//
+//
+//        }
+    
+    func setupARView(){
+          arView.automaticallyConfigureSession = false
+          
+          let config = ARWorldTrackingConfiguration()
+          config.planeDetection = [.horizontal,.vertical]
+          config.environmentTexturing = .automatic
+          config.frameSemantics = .personSegmentationWithDepth
+          config.isCollaborationEnabled = true
+          
+          arView.session.run(config)
+      }
 }
